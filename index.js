ad5332e75d6c5eb55ca157141b11ca7881d71764
Virginnboy <virginboyudemy@gmail.com>
2026-03-15 00:24:22 +0100

    modified allowed origins

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 // Middlewares
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: ["http://localhost:5173", "http://localhost:5174", "https://sky-kiddies-admin.onrender.com"], credentials: true}));
+app.use(cors({origin: ["http://localhost:5173", "http://localhost:5174", "https://sky-kiddies-admin.onrender.com", "https://sky-kiddies-client-end.onrender.com"], credentials: true}));
 
 app.get("/", (req, res) => {
   res.send("API is running");

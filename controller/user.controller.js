530d302155d2dfeae286264801d3a43e9594da12
Virginnboy <virginboyudemy@gmail.com>
2026-03-23 13:04:01 +0100

    optionally accept token through headers

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -57,19 +57,19 @@ const login = async (req, res) => {
     });
 
     try{
-      await sendEmail({
-      to: user.email,
-      subject: "SKy Kiddies Successful Login",
-      html: `
-        <h2>Dear ${user.firstName}</h2>
-        <p>You Have Loggedin successfully to Sky Kiddies on ${new Date().toLocaleString()}.</p>
-        <p>please if you do not intitiate this action, contact our customer support as your account might have been compromised.</p>
+      sendEmail({
+        to: user.email,
+        subject: "SKy Kiddies Successful Login",
+        html: `
+          <h2>Dear ${user.firstName}</h2>
+          <p>You Have Loggedin successfully to Sky Kiddies on ${new Date().toLocaleString()}.</p>
+          <p>please if you do not intitiate this action, contact our customer support as your account might have been compromised.</p>
 
 
-        
-        <p>Kind Regards,</p>
-        <p>Sky Kiddies Team</p>
-      `
+          
+          <p>Kind Regards,</p>
+          <p>Sky Kiddies Team</p>
+        `
     });
   }catch(emailError) {
     console.log("Email failed", emailError)
--- middleware/admin.auth.middleware.js
+++ middleware/admin.auth.middleware.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const protectAdmin = async (req, res, next) => {
   try {
-    const token = req.cookies?.adminToken;
+    const token = req.cookies?.adminToken || req.headers.authorization?.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({message: "Admin not authenticated"});
